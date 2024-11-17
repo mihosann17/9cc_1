36e433803d92d7fac9da1581d15155d2513c7bee
jentluna <09b15087@gmail.com>
2024-11-17 17:14:50 +0900

    トークナイザの補助関数を追加

--- 9cc.c
+++ 9cc.c
@@ -37,6 +37,41 @@ void error(char *fmt, ...)
     exit(1);
 }
 
+//  次のトークンが期待している記号の時には、トークンを1つ読み進めて
+//  真を返す。それ以外の場合には偽を返す。
+bool consume(char op)
+{
+    if(token->kind != TK_RESERVED || token->str[0] != op) return false;
+
+    token = token->next;
+    return true;
+}
+
+//  次のトークンが期待している記号の時には、トークンを1つ読み進める。
+//  それ以外の場合にはエラーを報告する。
+void expect(char op)
+{
+    if(token->kind != TK_RESERVED || token->str[0] != op) error("'%c'ではありません", op);
+
+    token = token->next;
+}
+
+//  次のトークンが数値の時には、トークンを1つ読み進めてその数値を返す。
+//  それ以外の場合にはエラーを報告する。
+int expect_number()
+{
+    if(token->kind != TK_NUM) error("数ではありません。");
+
+    int val = token->val;
+    token = token->next;
+    return val;
+}
+
+bool at_eof()
+{
+    return token->kind ==TK_EOF;
+}
+
 int main(int argc, char **argv)
 {
     if(argc != 2)
