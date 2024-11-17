8c1c343e37d973616ee4d31f2490692ea07394bb
jentluna <09b15087@gmail.com>
2024-11-17 17:00:51 +0900

    トークンの型とトークンの種類を定義

--- 9cc.c
+++ 9cc.c
@@ -1,5 +1,31 @@
+#include<ctype.h>
+#include<stdarg.h>
+#include<stdbool.h>
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+
+//トークンの種類
+typedef enum
+{
+    TK_RESERVED,    //  記号
+    TK_NUM,         //  整数トークン
+    TK_EOF,         //  入力の終わりを表すトークン
+}TokenKind;
+
+typedef struct Token Token;
+
+struct Token
+{
+    TokenKind kind; //  トークンの型
+    Token *next;    //  次の入力トークン
+    int val;        //  kindがTK_NUMの場合、その数値
+    char *str;      //  トークン文字列
+};
+
+//  現在着目しているトークン
+Token *token;
+
 
 int main(int argc, char **argv)
 {
